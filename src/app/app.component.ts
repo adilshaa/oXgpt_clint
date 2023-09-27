f72759a01f02ab2694c8f5de318bc13e94b2eeb9
adilshaa <adilsha042@gmail.com>
2023-09-27 11:39:04 +0530

    inital commit of oxgpt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ChatOxAi-clint';
+  chats: any[]=[];
+  chat_content!: string;
+  questionNumber: number = 0;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {}
+  sendChat() {
+    let chat = this.chat_content;
+    if (this.questionNumber >= 1) {
+      this.questionNumber++;
+    } else {
+      this.questionNumber = 1;
+    }
+    this.chats.unshift({
+      q_number: this.questionNumber,
+      response: '',
+      chat_qustion: chat,
+    });
+    this.chat_content = '';
+    this.http
+      .post(
+        `http://localhost:8000/inputs`,
+        { content: chat, number: this.questionNumber },
+        { withCredentials: true }
+      )
+      .subscribe((res: any) => {
+        console.log(res.q_no);
+        let isThere = this.chats.findIndex(
+          (items) => items.q_number == res.q_no
+        );
+        this.chats[isThere].response = res.chat;
+    console.log(this.chats[isThere]);
+
+      });
+  }
 }
